0059eca6ef1aa5ef4be88dd7c02c84656feaff04
Some14u <some1@email.ua>
2022-05-22 03:51:43 +0300

    Выполнены 2 и 3 задания

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,27 @@
+import throttle from "lodash.throttle";
+
+const iframe = document.getElementById("vimeo-player");
+
+const CURRENT_TIME_KEY = "videoplayer-current-time";
+const UPDATE_COOLDOWN = 1000; // milliseconds
+
+/**
+ * @type {import("@vimeo/player").Player}
+ */
+const player = new Vimeo.Player(iframe);
+
+var currentTime = localStorage.getItem(CURRENT_TIME_KEY);
+
+if (currentTime) {
+  player.setCurrentTime(currentTime);
+}
+
+player.on("timeupdate", throttle(onTimerUpdate, UPDATE_COOLDOWN));
+
+function onTimerUpdate({seconds}) {
+  localStorage.setItem(CURRENT_TIME_KEY, seconds);
+}
+
+
+
 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,47 @@
+import throttle from "lodash.throttle";
 
+const form = document.querySelector(".feedback-form");
+
+const LOCAL_STORAGE_KEY = "feedback-form-state";
+const UPDATE_COOLDOWN = 500; // milliseconds
+
+var state = loadStateFromLocalStorage();
+
+fillForm(state);
+
+form.addEventListener("input", throttle(onFormInput, UPDATE_COOLDOWN));
+
+form.addEventListener("submit", onFormSubmit);
+
+
+
+function onFormInput({target: {name, value}}) {
+  state[name] = value;
+  saveStateToLocalStorage(state);
+}
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  
+  console.table(state);
+
+  form.reset();
+  localStorage.removeItem(LOCAL_STORAGE_KEY);
+  state = {};
+}
+
+function fillForm(state) {
+  Object.keys(state).forEach(key => 
+    form.elements[key].value = state[key]
+  );
+}
+
+function loadStateFromLocalStorage() {
+  const key = localStorage.getItem(LOCAL_STORAGE_KEY);
+  return key ? JSON.parse(key) : {};
+}
+
+function saveStateToLocalStorage(state) {
+  const key = JSON.stringify(state);
+  localStorage.setItem(LOCAL_STORAGE_KEY, key);
+}
